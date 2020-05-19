5aa271299221fff1627dcde1059a55e89f0590c7
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2020-05-20 00:51:03 +0200

    scripts: update script that generating updated revs for libretro recipes.

--- scripts/libretro-create-hash-list.sh
+++ scripts/libretro-create-hash-list.sh
@@ -3,22 +3,43 @@
 ROOT="$(git rev-parse --show-toplevel)"
 
 IFS=$'\n'
+VERBOSE=${VERBOSE:-0}
+
+RESULT=()
+
+function export_from_recipe()
+{
+	recipe=$1
+	symbol=$2
+	default=$3
+	value=`grep $recipe -e ${symbol}`
+	value=${value// }
+	value=${value//\"}
+	eval ${value:-$symbol=$3}
+}
+
+function verbose_print()
+{
+	[[ "${VERBOSE}" == "1" ]] && echo -e "$*"
+}
 
 function get_pn()
 {
-	LIBRETRO_GIT_BRANCH="master"
-	repo=$1
-	to_eval=`grep ${repo%:*} -e 'LIBRETRO_GIT_BRANCH'`
-	to_eval=${to_eval// }
-	eval ${to_eval}
+	verbose_print "Entering recipe: ${1}"
+	unset LIBRETRO_GIT_BRANCH
+	unset LIBRETRO_GIT_REPO
+	package=${1%_git.bb}
+        package=${package##*/}
+	BPN="${package}"
+	export_from_recipe "${1}" LIBRETRO_GIT_BRANCH master
+	export_from_recipe "${1}" LIBRETRO_GIT_REPO
+	verbose_print "\tLIBRETRO_GIT_BRANCH=${LIBRETRO_GIT_BRANCH}"
+	verbose_print "\tLIBRETRO_GIT_REPO=${LIBRETRO_GIT_REPO}"
+	verbose_print "\tRECIPE=${package}"
 
-	package=${repo%_git.bb:*}
-	package=${package##*/}
+	[ -z "$LIBRETRO_GIT_REPO" ] && return 1
 
 	IFS=$' '
-	repo=${repo##*.bb:}
-	repo=${repo/\$\{BPN\}/${package}}
-	eval ${repo// }
 	hash=`git ls-remote https://${LIBRETRO_GIT_REPO} ${LIBRETRO_GIT_BRANCH} --refs`
 	hash=${hash%%ref*}
 	echo "LIBRETRO_GIT_REV_pn-${package} ?= \"${hash//[[:space:]]/}\""
@@ -26,8 +47,9 @@ function get_pn()
 
 function gen_from_dir()
 {
-	for repo in `grep -r $1 -e 'LIBRETRO_GIT_REPO'`; do
-		get_pn "$repo" &
+	for recipe in  `find ${1} -type f -iname "*-libretro_git.bb"`; do
+		get_pn "$recipe" &
+		sleep 0.20
 	done
 }
 
