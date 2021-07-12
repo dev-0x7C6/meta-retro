3f9c3863632de46f4f24e05438da92a95e9d3ecc
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2021-07-12 22:42:38 +0200

    scripts: fixed revision fetching.
    Signed-off-by: Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>

--- scripts/libretro-create-hash-list.py
+++ scripts/libretro-create-hash-list.py
@@ -14,9 +14,12 @@ with bb.tinfoil.Tinfoil() as tinfoil:
     for recipe in tinfoil.all_recipes():
         if 'libretro' in recipe.inherits() or 'libretro-cmake' in recipe.inherits():
             parsed = tinfoil.parse_recipe(recipe.pn)
-            print(recipe.pn)
-            bb.fetch2.get_srcrev(parsed)
-            ret.append('LIBRETRO_GIT_REV_pn-{0} ?= "{1}"'.format(recipe.pn, parsed.getVar('SRCREV')))
+            src_uris = parsed.getVar('SRC_URI')
+            src_uri = src_uris.split()[0]
+            ud = bb.fetch2.FetchData(src_uri, parsed)
+            revision = ud.method.latest_revision(ud, parsed, 'default')
+            ret.append('LIBRETRO_GIT_REV_pn-{0} ?= "{1}"'.format(recipe.pn, revision))
+            print(ret[-1])
 
     print("\nresults:\n")
     for value in sorted(ret): 
